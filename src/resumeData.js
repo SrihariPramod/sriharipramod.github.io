e4ae96a8a5a6cd71ad3806d1c487a68c3ff60550
sriharipramod <sriharipramod@gmail.com>
2019-06-06 23:14:11 +0530

    after npm

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,7 +1,7 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
+    "imagebaseurl":"https://sriharipramod.github.io/",
+    "name": "Srihari Pramod",
+    "role": "Data Scientist",
     "linkedinId":"Your LinkedIn Id",
     "skypeid": "Your skypeid",
     "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
